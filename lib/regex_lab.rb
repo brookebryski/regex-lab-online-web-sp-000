a95f9cea0138ad975a8e2d5a2a88d84ae5eac082
chic-state-0895 <brookeabryski@gmail.com>
2020-08-26 21:00:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,38 @@
+
+
 def starts_with_a_vowel?(word)
-word.match(/\b[aeiou]/i)
+
+  if word.match(/^[aeiouAEIOU]+\w/) then
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- text.scan(/un\w+ing/i)
+
+    text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\b[a-z]{5}\b/i)
+
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].+\.$/)
+
+  if text.match(/^[A-Z].+[\.!?]$/) then
+    return true
+  else
+    return false
+  end
 end
 
 def valid_phone_number?(phone)
- phone.match(/^[(]?\d{3}[)]?|\s]?\d{3}[\s-]?\d{4}|$/) 
+
+  if phone.match(/([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/) then
+    return true
+  else
+    return false
+  end
 end
