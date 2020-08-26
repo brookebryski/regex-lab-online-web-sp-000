9617243c3b77ff65a4dcc0c76c0f1b9b8bdd2068
chic-state-0895 <brookeabryski@gmail.com>
2020-08-26 20:54:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-word.match(/\b[aeiou]/i)
+word.match(/\b[aeiou]/i) 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,9 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].+\.$/)
+  text.match(/^[A-Z].+\.$/) 
 end
 
 def valid_phone_number?(phone)
-
+ phone.match(/^[(]?\d{3}[)]?|\s]?\d{3}[\s-]?\d{4}|$)
 end
